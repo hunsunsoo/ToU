d4b02974569bcb9e7b15c697a9d99c040f75d89e
DongIkkk <w8h0412@gmail.com>
2023-10-25 17:33:07 +0900

    Feat: Company, Worker Entity 작성

--- back/tou/src/main/java/com/welcome/tou/client/domain/Company.java
+++ back/tou/src/main/java/com/welcome/tou/client/domain/Company.java
@@ -1,7 +1,6 @@
 package com.welcome.tou.client.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,4 +10,28 @@ import lombok.NoArgsConstructor;
 @Table(name = "COMPANY")
 public class Company {
 
+    // 사업체 일련번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_seq", unique = true, nullable = false)
+    private Long companySeq;
+
+    // 업체명
+    @Column(name = "company_name", length = 50, nullable = false)
+    private String companyName;
+
+    // 사업자 등록 번호
+    @Column(name = "registration_number", length = 20, nullable = false)
+    private String registrationNumber;
+
+    // 사업체 소재지
+    @Column(name = "company_location", nullable = false)
+    private String companyLocation;
+
+    // 사업체 연락처
+    @Column(name = "company_contact", length = 20, nullable = false)
+    private String companyContact;
+
+    @Column(name = "logo_image", length = 200)
+    private String logoImage;
 }
--- back/tou/src/main/java/com/welcome/tou/client/dto/CompanyDto.java
+++ back/tou/src/main/java/com/welcome/tou/client/dto/CompanyDto.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class CompanyDto {
 }
