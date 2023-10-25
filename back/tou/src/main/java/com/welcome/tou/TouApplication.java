14eeb94dd2aaf4d268ac40437ef30a1b2e341ad0
DongIkkk <w8h0412@gmail.com>
2023-10-25 11:25:18 +0900

    Init Project

---
+++ back/tou/src/main/java/com/welcome/tou/TouApplication.java
@@ -0,0 +1,13 @@
+package com.welcome.tou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouApplication.class, args);
+	}
+
+}
