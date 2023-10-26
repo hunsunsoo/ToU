06c7b72d660d83daaad083b6d4e257bbb43368e4
DongIkkk <w8h0412@gmail.com>
2023-10-26 12:55:57 +0900

    Feat: ResultTemplate 추가

---
+++ back/tou/src/main/java/com/welcome/tou/common/utils/ResultTemplate.java
@@ -0,0 +1,13 @@
+package com.welcome.tou.common.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ResultTemplate<T> {
+    private int status;
+    private T data;
+}
