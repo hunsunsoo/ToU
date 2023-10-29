24214ef5194d9250cffda0e4499a80136d04074c
DongIkkk <w8h0412@gmail.com>
2023-10-30 02:12:29 +0900

    Feat: Spring Security Configuration 초기설정, Filter Chain 권한 test

--- back/tou/src/main/java/com/welcome/tou/security/config/SecurityConfig.java
+++ back/tou/src/main/java/com/welcome/tou/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.welcome.tou.security.config;
 
+import com.welcome.tou.client.domain.WorkerRepository;
 import com.welcome.tou.security.jwt.filter.JwtAuthenticationProcessingFilter;
 import com.welcome.tou.security.jwt.service.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,10 +21,33 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Slf4j
 public class SecurityConfig {
 
-    private  final JwtService jwtService;
+    private final JwtService jwtService;
+    private final WorkerRepository workerRepository;
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .httpBasic().disable()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/client/login").permitAll()
+                .requestMatchers("/api/client/company").hasRole("SELLER")
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationProcessingFilter(jwtService, workerRepository), UsernamePasswordAuthenticationFilter.class);
+
+        return http.getOrBuild();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
