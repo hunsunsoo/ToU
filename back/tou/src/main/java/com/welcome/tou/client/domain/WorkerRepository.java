dd476c9c313f5b05efcca0a593737cb99970a876
DongIkkk <w8h0412@gmail.com>
2023-10-30 02:09:00 +0900

    Feat: jwt 토큰 로그인을 위한 Worker Entity, Dto 추가

--- back/tou/src/main/java/com/welcome/tou/client/domain/WorkerRepository.java
+++ back/tou/src/main/java/com/welcome/tou/client/domain/WorkerRepository.java
@@ -2,5 +2,9 @@ package com.welcome.tou.client.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+    Optional<Worker> findByLoginId(String loginId);
 }
