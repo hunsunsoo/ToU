592f191d7ef1a4bffef3ad5ea2d8ea57fadb9b30
DongIkkk <w8h0412@gmail.com>
2023-11-01 13:01:18 +0900

    Feat: 거래 명세서 등록 API

--- back/tou/src/main/java/com/welcome/tou/statement/controller/StatementController.java
+++ back/tou/src/main/java/com/welcome/tou/statement/controller/StatementController.java
@@ -1,6 +1,13 @@
 package com.welcome.tou.statement.controller;
 
+import com.welcome.tou.common.utils.ResultTemplate;
+import com.welcome.tou.statement.dto.request.StatementCreateRequestDto;
+import com.welcome.tou.statement.service.StatementService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/api/statement")
 public class StatementController {
+
+    private final StatementService statementService;
+
+    @PostMapping("/worker")
+    public ResultTemplate<?> addStatement(@RequestBody StatementCreateRequestDto request) {
+        return statementService.addStatement(request);
+    }
 }
---
+++ back/tou/src/main/java/com/welcome/tou/statement/domain/ItemRepository.java
@@ -0,0 +1,6 @@
+package com.welcome.tou.statement.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
