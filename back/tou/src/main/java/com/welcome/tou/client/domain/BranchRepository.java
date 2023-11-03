ead0d4f160a6faad2ecf9cf2afb5b5f6904b3854
SquareOf_y <y6357@naver.com>
2023-11-03 11:00:16 +0900

    회사 pk에 따른 관할구역 목록 조회

--- back/tou/src/main/java/com/welcome/tou/client/domain/BranchRepository.java
+++ back/tou/src/main/java/com/welcome/tou/client/domain/BranchRepository.java
@@ -1,6 +1,14 @@
 package com.welcome.tou.client.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+    @Query("select b from Branch b "
+           +"where b.company.companySeq = :companySeq")
+    List<Branch> findByCompanySeq(@Param("companySeq") Long companySeq);
 }
