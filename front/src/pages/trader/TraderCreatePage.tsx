39247df056fc93b404b75010755542c79a290f9b
정동교 <jdk8467@naver.com>
2023-11-07 14:33:02 +0900

    Feat: create, createItem페이지 병합

--- front/src/pages/trader/TraderCreatePage.tsx
+++ front/src/pages/trader/TraderCreatePage.tsx
@@ -5,14 +5,16 @@ import { useState, useEffect } from "react";
 import { MainPaddingContainer } from "../../commons/style/mobileStyle/MobileLayoutStyle";
 import TraderSubtitle from "../../components/atoms/trader/TraderSubtitle";
 import TraderHeader from "../../components/organisms/trader/TraderHeader";
+import TraderItemHeader from "../../components/organisms/trader/TraderItemHeader";
 import TraderInputTitle from "../../components/organisms/trader/TraderInputTitle";
 import TraderInfoTitle from "../../components/organisms/trader/TraderInfoTitle";
 import TraderCalendarTitle from "../../components/organisms/trader/TraderCalendarTitle";
 import TraderDropdownTitle from "../../components/organisms/trader/TraderDropdownTitle";
 import TraderBtn from "../../components/atoms/trader/TraderBtn";
+import TraderUnitInputTitle from "../../components/organisms/trader/TraderUnitInputTitle";
 import { customAxios } from "../../components/api/customAxios";
 
-interface Item {
+interface DropdownItem {
   seq: number;
   name: string;
 }
@@ -27,8 +29,17 @@ interface Branch {
   branchName: string;
 }
 
+interface Item {
+  unit: string;
+  price: string;
+  amount: string;
+  note: string;
+}
+
 const TraderCreatePage = () => {
   const navigate = useNavigate();
+
+  const [reqStep, setReqStep] = useState(1);
 
   const [companyName, setCompanyName] = useState("");
   const [isValid, setIsValid] = useState(false); // 모든 입력값이 유효한지에 대한 상태
@@ -64,18 +75,47 @@ const TraderCreatePage = () => {
     new Date()
   );
 
-  const [selectedCompany, setSelectedCompany] = useState<Item | null>(null);
-  const [selectedBranch, setSelectedBranch] = useState<Item | null>(null);
+  const [selectedCompany, setSelectedCompany] = useState<DropdownItem | null>(null);
+  const [selectedBranch, setSelectedBranch] = useState<DropdownItem | null>(null);
 
-  const handleSelectCompany = (item: Item) => {
-    setSelectedCompany(item);
-    setSelectedCompanySeq(item.seq);
+  const [showAddButton, setShowAddButton] = useState(true);
+  const [showNextButton, setShowNextButton] = useState(true);
+  const [items, setItems] = useState<Item[]>([
+    {
+      unit: "",
+      price: "",
+      amount: "",
+      note: "",
+    },
+  ]);
+
+
+  const handleSelectCompany = (dropdownItem: DropdownItem) => {
+    setSelectedCompany(dropdownItem);
+    setSelectedCompanySeq(dropdownItem.seq);
   };
 
-  const handleSelectBranch = (item: Item) => {
-    setSelectedBranch(item);
-    setSelectedBranchSeq(item.seq);
+  const handleSelectBranch = (dropdownItem: DropdownItem) => {
+    setSelectedBranch(dropdownItem);
+    setSelectedBranchSeq(dropdownItem.seq);
   };
+
+  const checkValidity = () => {
+    const isCompanySelected = selectedCompany !== null;
+    const isBranchSelected = selectedBranch !== null;
+    const isDateSelected = selectedDate instanceof Date && !isNaN(selectedDate.valueOf());
+   
+    setIsValid(isCompanySelected && isBranchSelected && isDateSelected);
+  };
+
+  const nextHandler = (() => {
+    setReqStep(prevReqStep => prevReqStep + 1);
+  })
+
+  const goBackToStep1 = () => {
+    setReqStep(1); // 예를 들어, reqStep을 1로 설정하는 함수입니다.
+  };
+
   useEffect(() => {
     // 업체 목록 조회 API
     customAxios("/client/worker/company/list").then((res) => {
@@ -95,65 +135,196 @@ const TraderCreatePage = () => {
     }
   }, [selectedCompanySeq]);
 
-  const checkValidity = () => {
-    const isCompanySelected = selectedCompany !== null;
-    const isBranchSelected = selectedBranch !== null;
-    const isDateSelected = selectedDate instanceof Date && !isNaN(selectedDate.valueOf());
-   
-    setIsValid(isCompanySelected && isBranchSelected && isDateSelected);
-  };
-
   useEffect(() => {
     checkValidity();
   }, [selectedCompany, selectedBranch, selectedDate]);
 
-  // useEffect(() => {
-  //   console.log(selectedDate);
-  // }, [selectedDate]);
+
+  useEffect(() => {
+    if (items.length >= 20) {
+      setShowAddButton(false);
+    }
+  }, [items]);
+
+  useEffect(() => {
+    const allValid = items.every(
+      (item) => item.unit && item.price && item.amount && item.note
+    );
+    setShowAddButton(allValid);
+  }, [items]);
+
+  useEffect(() => {
+    const allValid =
+      items.length > 0 &&
+      items.every(
+        (item) => item.unit && item.price && item.amount && item.note
+      );
+    setShowNextButton(allValid);
+  }, [items]);
+
+  const handleInputChange = (
+    index: number,
+    field: keyof Item,
+    value: string
+  ) => {
+    setItems((prevItems) => {
+      return prevItems.map((item, idx) => {
+        if (idx !== index) {
+          return item;
+        }
+        return {
+          ...item,
+          [field]: value,
+        };
+      });
+    });
+  };
+
+  const handleItemClick = (billId: number) => {
+    navigate(`/m/confirm/${billId}`);
+  };
+
+  const addItem = () => {
+    setItems((prevItems) => [
+      ...prevItems,
+      { unit: "", price: "", amount: "", note: "" },
+    ]);
+  };
+
+  const removeItem = (index: number) => {
+    setItems((prevItems) => prevItems.filter((_, i) => i !== index));
+  };
 
   return (
-    <StyledContainer>
-      <StyledHeader>
-        <TraderHeader title="거래 명세서 생성" />
-        <TraderSubtitle subtitle="거래 업체 등록" />
-      </StyledHeader>
+    <>
+       {reqStep === 1 ? (
+          <StyledContainer>
+          <StyledHeader>
+            <TraderHeader title="거래 명세서 생성" />
+            <TraderSubtitle subtitle="거래 업체 등록" />
+          </StyledHeader>
+    
+          <StyledBody>
+            <MainPaddingContainer>
+              <TraderInfoTitle infoTitle="인수자 정보" />
+              <TraderDropdownTitle
+                inputTitle="업체명"
+                items={companyDropdownItems}
+                selectedItem={selectedCompany}
+                onSelect={handleSelectCompany}
+              />
+              <TraderDropdownTitle
+                inputTitle="관할 구역"
+                items={branchDropdownItems}
+                selectedItem={selectedBranch}
+                onSelect={handleSelectBranch}
+              />
+              <TraderInfoTitle infoTitle="거래 일자 등록" />
+              <TraderCalendarTitle
+                selectedDate={selectedDate}
+                onDateChange={setSelectedDate}
+              />
+            </MainPaddingContainer>
+          </StyledBody>
+    
+          <StyledFooter>
+            <TraderBtn
+              size="Large"
+              color={isValid ? "Blue" : "Grey"}
+              onClick={nextHandler}
+              disabled={!isValid}
+            >
+              다음
+            </TraderBtn>
+          </StyledFooter>
+        </StyledContainer>
+        ) : reqStep === 2 ? (
+          <StyledItemContainer>
 
-      <StyledBody>
-        <MainPaddingContainer>
-          <TraderInfoTitle infoTitle="인수자 정보" />
-          <TraderDropdownTitle
-            inputTitle="업체명"
-            items={companyDropdownItems}
-            selectedItem={selectedCompany}
-            onSelect={handleSelectCompany}
-          />
-          <TraderDropdownTitle
-            inputTitle="관할 구역"
-            items={branchDropdownItems}
-            selectedItem={selectedBranch}
-            onSelect={handleSelectBranch}
-          />
-          <TraderInfoTitle infoTitle="거래 일자 등록" />
-          <TraderCalendarTitle
-            selectedDate={selectedDate}
-            onDateChange={setSelectedDate}
-          />
-        </MainPaddingContainer>
-      </StyledBody>
-
-      <StyledFooter>
-        <TraderBtn
-          size="Large"
-          color={isValid ? "Blue" : "Grey"}
-          onClick={() => {
-            navigate("/m/create/item");
-          }}
-          disabled={!isValid}
-        >
-          다음
-        </TraderBtn>
-      </StyledFooter>
-    </StyledContainer>
+          <StyledItemHeader>
+            <TraderItemHeader title="거래 명세서 생성" onBack={goBackToStep1} />
+            <TraderSubtitle subtitle="거래 품목 등록" />
+          </StyledItemHeader>
+    
+          <StyledItemBody>
+            <MainPaddingContainer>
+              {items.map((item, index) => (
+                <div key={index}>
+                  <StyledInfoTitle>
+                    <TraderInfoTitle infoTitle="품목 정보" />
+                    <StyledSpan>
+                      ( {index + 1} / {items.length} )
+                    </StyledSpan>
+                  </StyledInfoTitle>
+                  {/* <TraderDropdownTitle inputTitle="품목" /> */}
+                  <TraderUnitInputTitle
+                    inputTitle="수량"
+                    value={item.unit}
+                    onChange={(e) =>
+                      handleInputChange(index, "unit", e.target.value)
+                    }
+                  />
+                  <TraderInputTitle
+                    inputTitle="단가"
+                    size="Large"
+                    value={item.price}
+                    onChange={(e) =>
+                      handleInputChange(index, "price", e.target.value)
+                    }
+                  />
+                  <TraderInputTitle
+                    inputTitle="금액"
+                    size="Large"
+                    value={item.amount}
+                    onChange={(e) =>
+                      handleInputChange(index, "amount", e.target.value)
+                    }
+                  />
+                  <StyledTraderInputTitle
+                    inputTitle="비고"
+                    size="X-Large"
+                    value={item.note}
+                    onChange={(e) =>
+                      handleInputChange(index, "note", e.target.value)
+                    }
+                  />
+                  <StyledDeleteItem>
+                    <TraderBtn
+                      size="Small"
+                      color="White"
+                      onClick={() => removeItem(index)}
+                    >
+                      삭제
+                    </TraderBtn>
+                  </StyledDeleteItem>
+                </div>
+              ))}
+              {showAddButton && (
+                <StyledAddItemButton>
+                  <TraderBtn size="X-Medium" color="Transparent" onClick={addItem}>
+                    + 품목 추가
+                  </TraderBtn>
+                </StyledAddItemButton>
+              )}
+            </MainPaddingContainer>
+          </StyledItemBody>
+    
+          <StyledFooter>
+            <TraderBtn
+              size="Large"
+              color={showNextButton ? "Blue" : "Grey"}
+              onClick={() => handleItemClick(2)} // 여기 billId 받아야 함
+              disabled={!showNextButton}
+            >
+              다음
+            </TraderBtn>
+          </StyledFooter>
+    
+        </StyledItemContainer>
+        ) : null
+      }
+  
+    </>
   );
 };
 
@@ -170,8 +341,68 @@ const StyledHeader = styled.div`
 
 const StyledBody = styled.div``;
 
+
+// 공통
 const StyledFooter = styled.div`
   width: 100%;
   position: fixed;
   bottom: 0;
+`;
+
+
+
+////  itemPage
+
+const StyledItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const StyledItemHeader = styled.div`
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 12;
+`;
+
+const StyledItemBody = styled.div`
+  padding-bottom: 70px;
+`;
+
+const StyledDeleteItem = styled.div`
+  padding-top: 30px;
+  padding-bottom: 30px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid var(--festie-gray-600, #010000);
+`;
+
+const StyledAddItemButton = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledInfoTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledSpan = styled.span`
+  display: inline-flex;
+  align-items: center;
+  height: 4vh;
+  width: 80px;
+  font-size: 20px;
+  font-weight: bold;
+  border-bottom: 0.8px solid var(--festie-gray-600, #949494);
+`;
+
+const StyledTraderInputTitle = styled(TraderInputTitle)`
+  > div {
+    align-items: flex-start;
+    margin: 0;
+  }
 `;
