20f2b44facb0241fb15077ef65538048eba9e549
김정훈 <junghun2581@naver.com>
2023-11-07 10:06:46 +0900

    Modify: 거래명세서 목록 조회 API 수정

--- front/src/components/atoms/officer/OfficerDocTable.tsx
+++ front/src/components/atoms/officer/OfficerDocTable.tsx
@@ -11,7 +11,7 @@ interface statementList {
   otherWorkerName?: string;
   productName: string;
   totalPrice?: number;
-  tradeDate?: Date;
+  tradeDate?: string;
   statementStatus: string;
 }
 
@@ -32,16 +32,6 @@ interface OfficerDocTableProps {
   isSupply: boolean;
   params: paramConfig;
 }
-
-// const items: Item[] = [
-//   { id: 1, name: "(주)싸피수산", person: "김싸피", item: "고등어 외 3건", supplyAmount: 2000000, date: new Date("2023-10-18"), state: "거래 예정" },
-//   { id: 2, name: "(주)수연수산", person: "이수연", item: "광어 외 2건", supplyAmount: 1500000, date: new Date("2023-09-12"), state: "거래 완료" },
-//   { id: 3, name: "(주)동현수산", person: "김동현", item: "오징어 외 4건", supplyAmount: 2000000, date: new Date("2023-08-30"), state: "거래 완료" },
-//   { id: 4, name: "(주)정훈수산", person: "김정훈", item: "고등어 외 1건", supplyAmount: 2000000, date: new Date("2023-06-30"), state: "거래 완료" },
-//   { id: 5, name: "(주)동익수산", person: "김동익", item: "연어", supplyAmount: 2000000, date: new Date("2023-04-12"), state: "거래 완료" },
-//   { id: 6, name: "(주)윤영수산", person: "황윤영", item: "우럭", supplyAmount: 2000000, date: new Date("2023-02-22"), state: "거래 완료" },
-//   { id: 7, name: "(주)동교수산", person: "정동교", item: "굴비", supplyAmount: 2000000, date: new Date("2022-12-29"), state: "거래 완료" },
-// ];
 
 const OfficerDocTable: React.FC<OfficerDocTableProps> = ({ isSupply, params  }) => {
   const navigate = useNavigate();
@@ -72,23 +62,10 @@ const OfficerDocTable: React.FC<OfficerDocTableProps> = ({ isSupply, params  }) 
       });
     };
 
-    // 거래명세서 목록 조회
-    // const param: paramConfig = {
-    //   page: 1, // 페이지 번호
-    //   type: isSupply ? "req" : "res", // 공급일 때, 수급일때 ( 내 회사가 공급 / 내 회사가 수급) req / res
-    //   companyName: "", // 업체명
-    //   isMine: true, // 내 거래만 보기
-    //   myWorkerName: null, // 본사 담당자
-    //   otherWorkerName: "", // 거래처 담당자
-    //   productName: "",
-    //   startDate: "",
-    //   endDate: "",
-    //   status: "",
-    // }
     customAxios.get(`statement/worker/list/web`, { params: params })
       .then((res) => {
-        console.log(res.data.data.statementList);
-        // setStatements(res.data.data.statementList);
+        // console.log(res.data.data.statementList);
+        setStatements(res.data.data.statementList);
       })
     }, [params]);
 
@@ -117,7 +94,7 @@ const OfficerDocTable: React.FC<OfficerDocTableProps> = ({ isSupply, params  }) 
             )}
             <td>{statement.productName}</td>
             <td>{statement.totalPrice}</td>
-            <td>{statement.tradeDate?.toLocaleDateString()}</td>
+            <td>{statement.tradeDate}</td>
             <td>{statement.statementStatus}</td>
           </tr>
         ))}
