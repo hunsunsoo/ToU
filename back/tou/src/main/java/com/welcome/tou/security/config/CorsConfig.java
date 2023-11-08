7b31d9c04e8e4497dbc5e6d82af5f4e5b0b3808b
DongIkkk <w8h0412@gmail.com>
2023-11-08 12:43:45 +0900

    Modify: CORS config 수정

--- back/tou/src/main/java/com/welcome/tou/security/config/CorsConfig.java
+++ back/tou/src/main/java/com/welcome/tou/security/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://tou.kr", "http://localhost:8080"));
+        config.setAllowedOrigins(List.of("https://tou.kr", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
