94ba4221496f052e316414f67af7a81e8087cc20
김정훈 <junghun2581@naver.com>
2023-11-08 11:24:59 +0900

    HOTFIX: Proxy 수정

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(    
     createProxyMiddleware('/api',{
-      target: 'http://tou.kr',
+      target: 'http://k9b310.p.ssafy.io:8080',
       changeOrigin: true,
     })
   );
