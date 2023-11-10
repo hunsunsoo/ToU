0d89178ba291ce658420da5535949d7b53f9a399
김동현 <gkfdkdlel@naver.com>
2023-11-10 21:28:08 +0900

    Modify: AssetTransfer

--- fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTest.java
+++ fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
 
 public final class AssetTest {
 
