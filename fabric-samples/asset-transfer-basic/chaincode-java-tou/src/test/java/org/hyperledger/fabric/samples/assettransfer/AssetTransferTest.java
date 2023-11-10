d33839e1e85c053cdf48dbe1666bb6f618fbc478
김동현 <gkfdkdlel@naver.com>
2023-11-10 11:15:27 +0900

    Modify: AssetTransferTest.java

--- fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTransferTest.java
+++ fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTransferTest.java
@@ -4,25 +4,25 @@
 
 package org.hyperledger.fabric.samples.assettransfer;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+//import static org.mockito.Mockito.inOrder;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.verifyZeroInteractions;
+//import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//import java.util.List;
 
-import org.hyperledger.fabric.contract.Context;
-import org.hyperledger.fabric.shim.ChaincodeException;
-import org.hyperledger.fabric.shim.ChaincodeStub;
-import org.hyperledger.fabric.shim.ledger.KeyValue;
-import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.mockito.InOrder;
+//import org.hyperledger.fabric.contract.Context;
+//import org.hyperledger.fabric.shim.ChaincodeException;
+//import org.hyperledger.fabric.shim.ChaincodeStub;
+//import org.hyperledger.fabric.shim.ledger.KeyValue;
+//import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+//import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InOrder;
 
 public final class AssetTransferTest {
 
