ea20f70ae10e22075e434def3e256a393734b1c9
이수연 <dltd0912@gmail.com>
2023-11-10 21:05:19 +0900

    Modify: 사업자 모바일 main 수정

--- front/src/components/organisms/trader/TraderMainProfile.tsx
+++ front/src/components/organisms/trader/TraderMainProfile.tsx
@@ -29,9 +29,7 @@ const TraderMainProfile = () => {
 export default TraderMainProfile;
 
 const StyledMainContainer = styled.div`
-  background-image: linear-gradient(38deg, #297fff 3.34%, #98fff9 90.74%);
   height: 100%;
-  color: #fff;
   height: 40vh;
   border-radius: 0px 0px 50px 50px;
   display: flex;
