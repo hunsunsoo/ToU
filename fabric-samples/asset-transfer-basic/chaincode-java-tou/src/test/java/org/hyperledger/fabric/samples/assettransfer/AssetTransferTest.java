d4e293d84968002911dcb9eb7b75ff7698ff363b
김동현 <gkfdkdlel@naver.com>
2023-11-12 00:46:21 +0900

    Modify: 체인코드 수정

--- fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTransferTest.java
+++ fabric-samples/asset-transfer-basic/chaincode-java-tou/src/test/java/org/hyperledger/fabric/samples/assettransfer/AssetTransferTest.java
@@ -113,14 +113,14 @@ public final class AssetTransferTest {
             ChaincodeStub stub = mock(ChaincodeStub.class);
             when(ctx.getStub()).thenReturn(stub);
 
-            String assetJson = "{\"assetId\":\"asset1\",\"previousAssetId\":\"1\":,\"statementSeq\":1,\"branchSeq\":1,\"branchLocation\":\"산본\",\"branchName\":\"산본공장\",\"branchContact\":\"010-7387-7808\",\"stockName\":\"멸치\",\"stockQuantity\":1,\"stockUnit\":\"kg\",\"stockDate\":\"2023-04-10T00:00:00\",\"inoutStatus\":\"OUT\",\"useStatus\":\"UNUSED\"}";
+            String assetJson = "{\"assetId\":\"asset1\",\"previousAssetId\":\"1\",\"statementSeq\":1,\"branchSeq\":1,\"branchLocation\":\"산본\",\"branchName\":\"산본공장\",\"branchContact\":\"010-7387-7808\",\"stockName\":\"멸치\",\"stockQuantity\":1,\"stockUnit\":\"kg\",\"stockDate\":\"2023-04-10T00:00:00\",\"inoutStatus\":\"OUT\",\"useStatus\":\"UNUSED\"}";
             when(stub.getStringState("asset1")).thenReturn(assetJson);
 
             Asset asset = contract.ReadAsset(ctx, "asset1");
 
             Asset expectedAsset = new Asset("asset1", "1", 1L, 1L, "산본", "산본공장", "010-7387-7808", "멸치", 1L, "kg", "2023-04-10T00:00:00", "OUT", "UNUSED");
-            assertThat("PASS").isEqualTo("PASS");
-//            assertThat(asset).isEqualTo(expectedAsset);
+//            assertThat("PASS").isEqualTo("PASS");
+            assertThat(asset).isEqualTo(expectedAsset);
         }
 
         @Test
