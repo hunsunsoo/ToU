cf97294c42467fe7c5510a8deb8aca9cb1eda036
SquareOf_y <y6357@naver.com>
2023-11-12 21:50:02 +0900

    Fix: org1 registerEnroll.sh 수정

--- fabric-samples/test-network/organizations/fabric-ca/registerEnroll.sh
+++ fabric-samples/test-network/organizations/fabric-ca/registerEnroll.sh
@@ -2,9 +2,10 @@
 
 function createOrg1() {
   infoln "Enrolling the CA admin"
-  mkdir -p cd ..
+  mkdir -p organizations/peerOrganizations/org1.example.com/
 
-d
+  export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/org1.example.com/
+
   set -x
   fabric-ca-client enroll -u https://admin:adminpw@localhost:7054 --caname ca-org1 --tls.certfiles "${PWD}/organizations/fabric-ca/org1/ca-cert.pem"
   { set +x; } 2>/dev/null
