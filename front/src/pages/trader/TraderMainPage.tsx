73e1cb28949316be3b0766170c5f2addeb74c0a1
정동교 <jdk8467@naver.com>
2023-11-13 16:17:07 +0900

    Feat: 소비자모바일 로그아웃 기능 구현

--- front/src/pages/trader/TraderMainPage.tsx
+++ front/src/pages/trader/TraderMainPage.tsx
@@ -3,10 +3,24 @@ import { MainPaddingContainer } from "../../commons/style/mobileStyle/MobileLayo
 import TraderMainPageBtn from "../../components/organisms/trader/TraderMainPageBtn";
 import TraderMainProfile from "../../components/organisms/trader/TraderMainProfile";
 import TraderMainCount from "../../components/organisms/trader/TraderMainCount";
+import { useNavigate } from "react-router-dom";
+import {MdLogout} from "react-icons/md";
 
 const TraderMainPage = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    // 토큰 삭제
+    localStorage.removeItem("recoil-persist");
+    navigate('/m/login');
+    window.location.reload();
+  };
+
   return (
     <StyledContainer>
+        <StyledLogout>
+           <MdLogout size="1.5rem" color="#0a3145" onClick={logout}/>
+          </StyledLogout>
       <TraderMainProfile />
       <MainPaddingContainer>
         <TraderMainCount />
@@ -27,3 +41,9 @@ const StyledFooter = styled.div`
   text-align: center;
   color: #ccc;
 `;
+
+const StyledLogout = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: flex-end; 
+`
